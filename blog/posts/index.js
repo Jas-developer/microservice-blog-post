9e75cfd897674f2db6e41c01352791026ef21d7d
Jason <devop.jas@gmail.com>
2023-12-24 03:20:11 +0800

    comments feature done

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,12 +1,37 @@
 const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
 
 //express server
 const app = express();
+app.use(bodyParser.json());
+const posts = {
+  test: {
+    title: "Hello My Name is Jason and I am a fullstack Software Developer",
+    id: "1",
+  },
+  test2: {
+    title: "My techstack in the backend are MEN(MONGODB,EXPRESS,NODEJS)",
+    id: "2",
+  },
+};
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).json(posts[id]);
+});
 
 app.listen(4000, () => {
-  console.log(`Listening on 4000`);
+  console.log(`Listening on 4000`, posts);
 });
